34f99b592a2d639d1ca9d0849aaf34d4d1a90918
fralonra <fralonra@users.noreply.github.com>
2020-02-29 12:42:23 +0000

    deploy: eebed9e4b4bd66d0870c2d1920c28ce95ebb3be2

---
+++ sw.js
@@ -0,0 +1,1 @@
+!function(){"use strict";importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"),workbox.routing.registerRoute(/\.js$/,new workbox.strategies.StaleWhileRevalidate)}();
